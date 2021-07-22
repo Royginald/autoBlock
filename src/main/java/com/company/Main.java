faf6faef1fc49796a3435be6ffa44ef8ba1aa5cd
RoyMc <roy.mcintosh3141@outlook.com>
2021-07-21 22:58:49 -0300

    commit July 21/2021 10:58PM

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -8,44 +8,85 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Main {
 
-    static int x = 0;
-    static int y = 0;
+    public static void main(String[] args) throws IOException, InterruptedException {
+        BlockSequence bruv = new BlockSequence();
 
-    static WebDriver driver;
-    static Actions acts;
-    static ChromeOptions option = new ChromeOptions();
-
-    public static void main(String[] args) throws IOException {
-//        option.addArguments("--headless");
-//        option.addArguments("--disable-extensions");
-        option.addArguments("--disable-gpu", "--window-size=1920,1080", "--start-maximized", "--ignore-certificate-errors", "--no-sandbox", "--disable-dev-shm-usage", "--disable-notifications", "--disable-infobars", "--disable-popup-blocking");
-
-        //this is to get the chrome driver even if its on a different operating system
-        //(Mostly user for if this program goes on a Heroku server
-        if(System.getProperty("os.name").equals("Windows 10")) {
-            String userPath = System.getProperty("user.dir");
-            System.setProperty("webdriver.chrome.driver", userPath + "/chromedriver.exe");
-        } else {
-            option.setBinary(EnvironmentUtils.getProcEnvironment().get("GOOGLE_CHROME_SHIM"));
-        }
-
-        //start the chrome driver
-        driver = new ChromeDriver(option);
-        acts = new Actions(driver);
-
-        String url = "http://google.com";
-        System.out.println("bruh");
-
-        driver.get(url);
+        bruv.init(1);
+        bruv.open("http://google.com");
+        bruv.type("vicious syndicate\n");
+        bruv.delay(5000);
+        bruv.close();
     }
 
-    public static void moveTo(int xPos, int yPos, int timeout) throws InterruptedException {
-        TimeUnit.MILLISECONDS.sleep(timeout);
-        acts.moveByOffset(xPos - x, yPos - y).click().build().perform();
-        x = xPos;
-        y = yPos;
+    //a series of commands to easily automate web tasks
+    static class BlockSequence {
+        //variables for later
+        int[] position = new int[]{0,0};
+        WebDriver driver;
+        Actions actions;
+        static ChromeOptions options = new ChromeOptions();
+
+        //run this to initiate the sequence
+        public void init(int editLevel) throws IOException {
+            switch (editLevel) {
+                case 0:
+                    options.addArguments("--headless");
+                case 1:
+                    options.addArguments("--disable-extensions");
+                case 2:
+                    options.addArguments("--disable-notifications");
+                default:
+                    options.addArguments("--disable-gpu", "--window-size=1920,1080", "--start-maximized", "--ignore-certificate-errors", "--no-sandbox", "--disable-dev-shm-usage", "--disable-infobars", "--disable-popup-blocking");
+            }
+
+            if(System.getProperty("os.name").equals("Windows 10")) {
+                String userPath = System.getProperty("user.dir");
+                System.setProperty("webdriver.chrome.driver", userPath + "/chromedriver.exe");
+            } else {
+                options.setBinary(EnvironmentUtils.getProcEnvironment().get("GOOGLE_CHROME_SHIM"));
+            }
+
+            driver = new ChromeDriver(options);
+            actions = new Actions(driver);
+        }
+
+        //open a new tab
+        public void open(String url) throws InterruptedException {
+            driver.get(url);
+            new WebDriverWait(driver, 30).until(ExpectedConditions.elementToBeClickable(By.xpath("/html")));
+        }
+
+        //close the current tab
+        public void close() {
+            driver.close();
+        }
+
+        //move to a point on a page
+        public void moveTo(int x, int y) {
+            actions.moveByOffset(x - position[0] , y - position[1]);
+            position = new int[] {x, y};
+        }
+
+        //click the current location
+        public void click(int times) {
+            for(int i = 0; i < times; i++) {
+                actions.click().build().perform();
+            }
+        }
+
+        //delay for a period of time in milliseconds
+        public void delay(int time) throws InterruptedException {
+            TimeUnit.MILLISECONDS.sleep(time);
+        }
+
+        //type text at the current location (note you may need to click to select the current location)
+        public void type(String text) {
+            actions.sendKeys(text).perform();
+        }
     }
 }
